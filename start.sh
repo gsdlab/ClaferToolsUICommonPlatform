f3af85706152f7ed475251b916fc57b1790c4e41
Michal Antkiewicz <mantkiew@gsd.uwaterloo.ca>
2014-01-10 16:09:02 -0500

    using sandbox
    .cabal-sandbox/bin

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 echo "Starting the server under the supervisor."
+echo "Running against ~/0.3.5/cabal-sandbox/bin"
+export PATH=~/0.3.5/.cabal-sandbox/bin:$PATH
 echo "Autorestarting on source code changes."
 echo "Polling for source code changes every minute."
 cd ..
-nohup supervisor -w -p60000 server.js & 
+nohup supervisor -w -p60000 server.js &
