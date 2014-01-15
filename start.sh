eebb0a963c8bace646791b759a8c8b74d88a6b1e
Michal Antkiewicz <mantkiew@gsd.uwaterloo.ca>
2014-01-14 23:32:12 -0500

    print clafer and IG versions in start script

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 echo "Starting the server under the supervisor."
 echo "Running against ~/0.3.5/cabal-sandbox/bin"
 export PATH=~/0.3.5/.cabal-sandbox/bin:$PATH
+clafer -V
+claferIG -V
 echo "Autorestarting on source code changes."
 echo "Polling for source code changes every minute."
 cd ..
